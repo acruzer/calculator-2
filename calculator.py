9cc0a1d6c8694be0e1f7cffce65ee1686c7aa84d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-24 17:16:33 -0700

    Added input string function

--- calculator.py
+++ calculator.py
@@ -11,13 +11,38 @@ from arithmetic import *
 # input_string = input ("Enter you Calculation:")
 
 # token = input_string.split(" ")
+def input_string():
+	input_string = input ("Enter your Calculation: ")
+	token = input_string.split(" ")
+	
+	return token
+
 def calculator():
-	input_string = input ("Enter you Calculation: ")
 
-	token = input_string.split(" ")	
-	if token[0] == "pow":
-		result = power (int(token[1]), int(token[2]))
-	# print (token)
+	while True:
+		token = input_string()	
+		operator = token[0]
+		num1 = int(token[1])
+		num2 = int(token[2])	
+		
+		if operator == "+":
+			result = add (num1, num2)
+				
+		elif operator == "-":
+			result = subtract (num1, num2)
+				
+		elif operator == "*":
+			result = multiply (num1, num2)
+
+		elif operator == "q":
+			break
+
+		# input_string = input ("Enter you Calculation: ")
+		# token = input_string.split(" ")
+
+		# print (token)
 		print (result)
+	# input_string()
 
+# input_string()
 calculator()
